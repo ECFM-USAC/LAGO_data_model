433a44920a9e6bdd4aa972044f7e118dfc97706f
Erick Diaz <ewdiazsa@gmail.com>
2025-10-09 12:40:54 -0600

    updated mlp model

--- lago_data_model/model_utils.py
+++ lago_data_model/model_utils.py
@@ -19,6 +19,34 @@ from sklearn.metrics import (
     accuracy_score
 )
 
+class EarlyStopping:
+    def __init__(self, patience=5, delta=0, model_name=None):
+        self.patience = patience
+        self.delta = delta
+        self.best_score = None
+        self.early_stop = False
+        self.counter = 0
+        self.model_name = model_name
+
+    def __call__(self, val_loss, model):
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''Save model when validation loss decrease.'''
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        torch.save(model.state_dict(), f"checkpoint_timestamp.pt")
 
 def eval_predictions(
     y_true,
